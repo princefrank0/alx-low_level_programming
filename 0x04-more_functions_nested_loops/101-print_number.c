6936ebc2dd47606ccedcfc0427b9429aebac6fcf
Dadi <princefrank005@gmail.com>
2022-07-08 12:15:34 +0100

    added commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,33 @@
-#include "mainn.h"
+#include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - prints a number to the screen
+ * @n: the given number
+ *
+ * Return: nothing
  */
 void print_number(int n)
 {
-unsigned int n1;
-
-if (n < 0)
-{
-n1 = -n;
-_putchar('-');
-}
-else
-{
-n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		print_unsigned_int(-(unsigned int)n);
+	}
+	else
+		print_unsigned_int(n);
 }
 
-if (n1 / 10)
+/**
+ * print_unsigned_int - prints an unsigned integer
+ * @n: given unsigned integer
+ *
+ * Return: nothing
+ */
+void print_unsigned_int(unsigned int n)
 {
-print_number(n1 / 10);
+	if (n / 10)
+		print_unsigned_int(n / 10);
+	_putchar((n % 10) + '0');
 }
 
-_putchar((n1 % 10) + '0');
-}
+
